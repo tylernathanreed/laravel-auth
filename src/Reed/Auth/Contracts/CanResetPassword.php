0e0c2ed2ee1dfe116c00c40dad91c79801f32f27
Tyler Reed <tyler.reed@haloco.com>
2016-11-11 09:03:51 -0600

    + Added CanResetPassword Contract

---
+++ src/Reed/Auth/Contracts/CanResetPassword.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Reed\Auth\Contracts;
+
+use Illuminate\Contacts\Auth\CanResetPassword as CanResetPasswordContract;
+
+interface CanResetPassword extends CanResetPasswordContract
+{
+
+}
